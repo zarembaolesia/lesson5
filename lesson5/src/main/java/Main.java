a40d6eaebfc2621be178244a4b5dcc8d1d3e7139
zarembaolesia <111783336+zarembaolesia@users.noreply.github.com>
2023-06-20 10:33:56 +0300

    Update Main.java
    Added a statement to print out the message to the console

--- lesson5/src/main/java/Main.java
+++ lesson5/src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         MyClass myClass = new MyClass();
-        myClass.getFieldValue();
+        int value = myClass.getFieldValue();
+        System.out.println("~~~ " + value + "~~~ ");
     }
 }
